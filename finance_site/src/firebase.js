b59a0c4118b397a408d9f1d6fd1d4062c44d1c2b
ebrahim-sani <ibraheemsani035@gmail.com>
2021-04-07 10:43:07 +0100

    Integrate Redux and Firebase into the Project

---
+++ finance_site/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyfCDPsUtED7MeHqfPAYoUS50jZIjKleU",
+  authDomain: "finance-1bb88.firebaseapp.com",
+  projectId: "finance-1bb88",
+  storageBucket: "finance-1bb88.appspot.com",
+  messagingSenderId: "246496157876",
+  appId: "1:246496157876:web:44c2d7d5031d822d7077e1",
+};
